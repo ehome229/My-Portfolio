47b0253cb6d324666622a89b0b7184ac370ff3b1
ehome229 <67464333+ehome229@users.noreply.github.com>
2022-03-06 09:59:44 +0100

    putting of youtube videos

--- index.php
+++ index.php
@@ -1,320 +1,79 @@
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Nestor's portfolio</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
-
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top">My portfolio</a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#acceuil">Home</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">My projetc</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#skills">My SKILLS</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Masthead-->
-        <header class="masthead bg-prim bg-dark pt-13 text-white text-center" id="acceuil">
-            <div class="container d-flex align-items-center flex-column  ">
-                <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar  w-25 my-0 py-0" src="assets/img/portfolio/nestor1.jpg" id="accueilimg" alt="..." />
-                <!-- Masthead Heading-->
-                <h1 class="masthead-subheading ext-uppercase mb-0">I'm  Ir. Nestor Kenoude Amouzoun</h1>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Programmer - Graphic designer - Maintainer - Electronic</p>
-            </div>
-            
-        </header>
-        <!-- Portfolio Section-->
-        <section class="page-section portfolio" id="portfolio">
-            <div class="container">
-                <!-- Portfolio Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">My projects</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Portfolio Grid Items
-                <div class="row justify-content-center">
-                   
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            
-                            <embed src="assets/video/video3.mp4" autostart="false" height="50%"  width="100%"/>  
-                        </div>
-                        <h3 class="text-center">Tadagbé</h3>
-                        <details>Tadagbe is a cohabitation agreement project that allows clients to register online, track their payments online and on the other hand allows the provider to store all clients in a database and manage their payments.
- </details>
-                    </div>
-               
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <embed src="assets/video/video1.mp4" autostart="false" height="50%"  width="100%"/>  
-                        </div>
-                        <h3 class="text-center">Covid Filter Part1</h3>
-                        <details>Covid Filter is an electronic project designed under artificial intelligence to serve maximum protection
-for the population. This project consists in ensuring the scope of the mask or not by a robot, temperature control and a tunnel of
-disinfection.</details>
-                    </div>
-                  
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
-                            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <!--<img class="img-fluid" src="assets/img/portfolio/circus.png" alt="..." />
-                            <embed src="assets/video/video2.mp4" autostart="false" height="50%"  width="100%"/>  
-                        </div>
-                        <h3 class="text-center">Covid Filter Part2</h3>
-                        <details>Covid Filter is an electronic project designed under artificial intelligence to serve maximum protection
-for the population. This project consists in ensuring the scope of the mask or not by a robot, temperature control and a tunnel of
-disinfection.</details> 
-                    </div>
-                   
-                <!-- </div>-- -->
-            </div>
-        </section>
-
-        <section class="page-section portfolio" id="skills">
-            <div class="container">
-                <!-- Portfolio Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">My SKILLS</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Portfolio Grid Items-->
-                <div class="row justify-content-center">
-
-                    <div class="col-md-6 col-lg-4 mb-5">
-                    <span>HTML5</span> <span class="pull-right">75%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-warning" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>CSS3</span> <span class="pull-right">65%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>PHP</span> <span class="pull-right">60%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>JavaScript</span> <span class="pull-right">50%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-secondary" role="progressbar" style="width: 50%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-</div>
-
-<div class="col-md-6 col-lg-4 mb-5">
-
-<span>Bootstrap</span> <span class="pull-right">70%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>Symfony</span> <span class="pull-right">60%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-danger" role="progressbar" style="width: 60%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>Laravel</span> <span class="pull-right">50%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-danger" role="progressbar" style="width: 60%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>OOP</span> <span class="pull-right">50%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-info" role="progressbar" style="width: 70%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-</div>
-
-<div class="col-md-6 col-lg-4 mb-5">
-<span>Photoshop</span> <span class="pull-right">80%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>Corel Draw</span> <span class="pull-right">60%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-
-                    <span>Electronics & Arduino</span> <span class="pull-right">70%</span>
-                    <div class="progress mb-5">
-                      <div class="progress-bar bg-success" role="progressbar" style="width: 65%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-</div>
-
-            
-
-
-
-                  </div>
-            </div>
-        </section>
-
-
-
-        <!-- About Section-->
-        <section class="page-section bg-dark text-white mb-0" id="about">
-            <div class="container">
-                <!-- About Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom divider-light">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- About Section Content-->
-                <div class="row">
-                    <div class="col-lg-4 ms-auto"><p class="lead">I am Nestor Kenoude Amouzoun, a computer engineer. In my primary school, I liked electronics a lot before I went to university where I discovered the world of programming which attracted me well.</p></div>
-                    <div class="col-lg-4 me-auto"><p class="lead">In short, I am passionate about the computer world and many of its derivatives such as: electronics, maintenance, programming, graphics, networking, ... .</p></div>
-                </div>
-                <!-- About Section Button-->
-                <div class="text-center mt-4">
-                    <a class="btn btn-xl btn-outline-light" download href="assets/img/portfolio/NestorCV.pdf">
-                        <i class="fas fa-download me-2"></i>
-                        My CV!
-                    </a>
-                </div>
-            </div>
-        </section>
-        <!-- Contact Section-->
-        <section class="page-section" id="contact">
-            <div class="container d-flex flex-column">
-                <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-           <!-- <h6 class="text-center">ehome229@gmail.com</h6>-->
-            <a href="mailto:ehome229@gmail.com" class="btn btn-outline-dark  mx-auto rounded align-items-center"> <i class="fab fa-fw fa-"></i> Send me mail from Here</a>
-            </div>
-        </section>
-        <!-- Footer-->
-        <footer class="footer text-center">
-            <div class="container">
-                <div class="row">
-                    <!-- Footer Location-->
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Location</h4>
-                        <p class="lead mb-0">
-                            Akpakpa-Cotonou, Benin Rep.
-                            <br />
-                            M/ AKONDE N°323
-                        </p>
-                    </div>
-                    <!-- Footer Social Icons-->
-                    <div class="col-lg-6 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Social Network</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://web.facebook.com/nestorkenoude.amouzoun"><i class="fab fa-fw fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                    </div>
-                    <!-- Footer About Text-->
-
-                </div>
-            </div>
-        </footer>
-        <!-- Copyright Section-->
-        <div class="copyright py-4 text-center text-white">
-            <div class="container"><small>Copyright &copy; Your Website 2022</small></div>
-        </div>
-        <!-- Portfolio Modals-->
-        <!-- Portfolio Modal 1-->
-
-        <!-- Portfolio Modal 6-->
-       
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-
-        <script>
-    var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?73483';
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = url;
-    var options = {
-  "enabled":true,
-  "chatButtonSetting":{
-      "backgroundColor":"#4dc247",
-      "ctaText":"",
-      "borderRadius":"25",
-      "marginLeft":"0",
-      "marginBottom":"50",
-      "marginRight":"50",
-      "position":"right"
-  },
-  "brandSetting":{
-      "brandName":"WATI",
-      "brandSubTitle":"Typically replies within a day",
-      "brandImg":"https://cdn.clare.ai/wati/images/WATI_logo_square_2.png",
-      "welcomeText":"Hi there!\nHow can I help you?",
-      "messageText":"Hello, I have a question about {{page_link}}",
-      "backgroundColor":"#0a5f54",
-      "ctaText":"Start Chat",
-      "borderRadius":"25",
-      "autoShow":false,
-      "phoneNumber":"22967583932"
+<?php
+  if (!empty($_GET['q'])) {
+    switch ($_GET['q']) {
+      case 'info':
+        phpinfo(); 
+        exit;
+      break;
+    }
   }
-};
-    s.onload = function() {
-        CreateWhatsappChatWidget(options);
-    };
-    var x = document.getElementsByTagName('script')[0];
-    x.parentNode.insertBefore(s, x);
-</script>
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laragon</title>
 
+        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Karla';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .opt {
+                margin-top: 30px;
+            }
+
+            .opt a {
+              text-decoration: none;
+              font-size: 150%;
+            }
+            
+            a:hover {
+              color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title" title="Laragon">Laragon</div>
+     
+                <div class="info"><br />
+                      <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
+                      PHP version: <?php print phpversion(); ?>   <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
+                      Document Root: <?php print ($_SERVER['DOCUMENT_ROOT']); ?><br />
+
+                </div>
+                <div class="opt">
+                  <div><a title="Getting Started" href="https://laragon.org/docs">Getting Started</a></div>
+                </div>
+            </div>
+
+        </div>
     </body>
 </html>
